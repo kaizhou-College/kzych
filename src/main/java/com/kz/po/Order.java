e972135d9e1cdead3762d6111efa06665c78fb5b
kate-chan <ccsu_kings@163.com>
2018-03-27 12:25:03 +0800

    跑通支付宝支付

---
+++ src/main/java/com/kz/po/Order.java
@@ -0,0 +1,116 @@
+package com.kz.po;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Order {
+    private Long id;
+
+    private Long uuid;
+
+    private Long orderNo;
+
+    private BigDecimal payment;
+
+    private Integer status;
+
+    private Integer paymentType;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Date endTime;
+
+    private Date closeTime;
+
+    private Date playmentTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(Long uuid) {
+        this.uuid = uuid;
+    }
+
+    public Long getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(Long orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public BigDecimal getPayment() {
+        return payment;
+    }
+
+    public void setPayment(BigDecimal payment) {
+        this.payment = payment;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(Integer paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Date getCloseTime() {
+        return closeTime;
+    }
+
+    public void setCloseTime(Date closeTime) {
+        this.closeTime = closeTime;
+    }
+
+    public Date getPlaymentTime() {
+        return playmentTime;
+    }
+
+    public void setPlaymentTime(Date playmentTime) {
+        this.playmentTime = playmentTime;
+    }
+}
--- src/main/java/com/kz/service/impl/OrderServiceImpl.java
+++ src/main/java/com/kz/service/impl/OrderServiceImpl.java
@@ -2,6 +2,8 @@ package com.kz.service.impl;
 
 import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,10 +23,13 @@ import com.kz.po.OrderDetail;
 import com.kz.po.OrderQuery;
 import com.kz.service.IOrderService;
 import com.kz.utils.ZfbPropertiesUtil;
+import com.kz.web.controller.portal.OrderController;
 
 @Service
 @Transactional
 public class OrderServiceImpl extends BaseService<Order, OrderQuery>implements IOrderService {
+	private Logger logger = LoggerFactory.getLogger(OrderController.class);
+	
 	private OrderMapper orderMapper = null;
 
 	@Autowired
@@ -66,19 +71,25 @@ public class OrderServiceImpl extends BaseService<Order, OrderQuery>implements I
 		AlipayTradeAppPayRequest request = new AlipayTradeAppPayRequest();
 		// SDK已经封装掉了公共参数，这里只需要传入业务参数。
 		// 以下方法为sdk的model入参方式(model和biz_content同时存在的情况下取biz_content)。
+		//1,得到订单对象
+		Order tmpOrder = orderMapper.selectByOrderNo(orderNo);
+		
+		
+		
 		AlipayTradeAppPayModel model = new AlipayTradeAppPayModel();
-		model.setBody("我是测试数据");
-		model.setSubject("App支付测试Java");
-		model.setOutTradeNo("201818181");
+		model.setBody("购买了欧乐B电动牙刷一只");
+		model.setSubject("电动牙刷");
+		model.setOutTradeNo(tmpOrder.getOrderNo().toString());
 		model.setTimeoutExpress("30m");
-		model.setTotalAmount("0.01");
+		model.setTotalAmount(tmpOrder.getPayment().toString());
 		model.setProductCode("QUICK_MSECURITY_PAY");
 		request.setBizModel(model);
 		// request.setNotifyUrl("商户外网可以访问的异步地 址");
 		try {
 			// 这里和普通的接口调用不同，使用的是sdkExecute
 			AlipayTradeAppPayResponse response = alipayClient.sdkExecute(request);
-			System.out.println(response.getBody());// 就是orderString
+			//System.out.println(response.getBody());// 就是orderString
+			logger.info("请求参数加签成功....");
 			return ServerResponse.createBySuccess("参数加签成功！", response.getBody());										// 可以直接给客户端请求，无需再做处理。
 		} catch (AlipayApiException e) {
 			e.printStackTrace();
